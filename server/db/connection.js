9e2d03986d22bbc6e12072bb988352121a2f3423
ppaun <ppaun@ibaset.com>
2024-02-15 10:39:25 +0530

    first commit

---
+++ server/db/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+// const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zw6hky5.mongodb.net/?retryWrites=true&w=majority`;
+const url = `mongodb://localhost:27017/react-chat-app`;
+
+mongoose.connect(url, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to DB')).catch((e)=> console.log('Error', e))
